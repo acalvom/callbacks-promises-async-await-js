f66ac3c909d73023b173fb87b2cd1cc81eb8640e
acalvom <andreac.3108@gmail.com>
2021-11-19 08:50:39 +0100

    Promises Hell

--- asincronia-js/promises.js
+++ asincronia-js/promises.js
@@ -12,9 +12,22 @@ const findPostById = (id) => {
 }
 
 findPostById(1)
-    .then((post) => {
-        console.log(post)
-    }) // Cuando ocurre el resolve
-    .catch(err => {
+    .then((post) => {// Cuando ocurre el resolve
+        console.log(post);
+
+        // Promises anidadas. Promises Hell. 
+        return findPostById(2)
+            .then(post => {
+                console.log(post);
+                return findPostById(3)
+                    .then(post => {
+                        console.log(post);
+                        return findPostById(4);
+                    })
+            })
+    })
+
+
+    .catch(err => {// Cuando ocurre el reject
         console.log(err)
-    }) // Cuando ocurre el reject
+    })
