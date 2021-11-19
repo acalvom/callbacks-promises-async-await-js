5dedeb13130b8b2c15710764dbcacc4367367509
acalvom <andreac.3108@gmail.com>
2021-11-19 12:51:28 +0100

    Async/Await: Add timeout & test sequence of promises

--- asincronia-js/asyncAwait.js
+++ asincronia-js/asyncAwait.js
@@ -5,20 +5,28 @@ import posts from './posts.js'
 
 const findPostById = (id) => {
     return new Promise((resolve, reject) => {
-        const post = posts.find(item => item.id === id);
 
-        if (post)
-            resolve(post);
-        else
-            reject("No post found with id: " + id);
+        // Añadir un timeout para comprobar el efecto del await
+        setTimeout(() => {
+            const post = posts.find(item => item.id === id);
+
+            if (post)
+                resolve(post);
+            else
+                reject("No post found with id: " + id);
+        }, 2000);
+
     });
 }
 
 // Nueva función async + Función con promesa await
 const findPostByIdAsync = async (id) => {
     try {
-        const post = await findPostById(id); // Awai siemrpre dentro de un await
-        console.log(post);
+        // Considerando ambas llamadas independientes una de otra
+        const post1 = await findPostById(1); // Await siempre dentro de un await
+        const post2 = await findPostById(2);
+
+        console.log(post1.title, post2.title); // El console no se ejecuta hasta que no te han obtenido los 2 posts (4 segundos)
     } catch (error) {
         console.log(error);
     } finally {
